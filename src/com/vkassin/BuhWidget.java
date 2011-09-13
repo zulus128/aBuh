79b3192ee15e85fe4c98a4a993d08f166243ee61
Vadim <vgzulus@gmail.com>
2011-09-13 17:26:11 +0600

    qqqqq

---
+++ src/com/vkassin/BuhWidget.java
@@ -0,0 +1,38 @@
+package com.vkassin;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class BuhWidget extends TabActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, NewsActivity.class);
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("news").setIndicator("Новости"/*,
+	                      res.getDrawable(R.drawable.ic_tab_news)*/)
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, QAActivity.class);
+	    spec = tabHost.newTabSpec("qas").setIndicator("Вопрос-ответ"/*,
+	                      res.getDrawable(R.drawable.ic_tab_qas)*/)
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+
+	    tabHost.setCurrentTab(1);
+	}
+}
